7ab442a50fd3650d41df23f05e886bf8d33c0b2e
KU10S <shamilvkage@gmail.com>
2023-04-10 17:02:47 +0530

    order api.

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Razorpay = require("razorpay")
 const crypto = require("crypto");
+const { error } = require("console");
 
 router.post("/orders",async(req, res) => {
     try {
@@ -8,8 +9,22 @@ router.post("/orders",async(req, res) => {
             key_id: process.env.KEY__ID,
             key_secret: process.env.KEY__SECRET
         })
-    } catch (error) {
 
+        const options = {
+            amount: request.body.amount*100,
+            currency: "INR",
+            receipt:crypto.randomBytes(10).toString("hex")
+        };
+
+        instance.orders.create(options, (err, order) => {
+            if (err) {
+                console.log(error);
+                return res.status(500).json({ error })
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server Error" })
     }
 });
 
