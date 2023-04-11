47560ebac3b50169be63b24041eb15d904828b8d
KU10S <shamilvkage@gmail.com>
2023-04-11 12:46:27 +0530

    product creation.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import axios from "axios"
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [book, setBook] = useState({
+    name: "Looking For Alaska",
+    author:"Jhon Green",
+    img:"https://bookbins.in/wp-content/uploads/2022/11/Looking-For-Alaska-John-Green-Buy-Online-Bookbins-1.png",
+    price:350
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="book_container">
+        <img src= {book.img} alt="book_img" className="book_img"/>
+          <p className="book_name">{book.name}</p>
+          <p className="book_author">By {book.author}</p>
+          <p className="book_price">
+            Price : <span>&#x20B9; {book.price}</span>
+          </p>
+          <button className="buy_btn">
+            Buy Now
+          </button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
